758fa0bd8bea6a8c3db312adb6b76ae0a0e383b5
imkh817 <rjs7180@naver.com>
2026-02-10 01:20:43 +0900

    Feat: 검색 API 테스트 코드 구현

---
+++ src/test/java/study/blog/global/IntegrationTestSupport.java
@@ -0,0 +1,31 @@
+package study.blog.global;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Transactional
+@SpringBootTest
+@Testcontainers
+public abstract class IntegrationTestSupport {
+
+    static final MySQLContainer<?> MYSQL_CONTAINER;
+
+    static {
+        MYSQL_CONTAINER = new MySQLContainer<>("mysql:8.0")
+                .withDatabaseName("test_db")
+                .withUsername("root")
+                .withPassword("test");
+        MYSQL_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+    }
+}
